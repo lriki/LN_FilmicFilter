34c2e6c2a3dea93dfedb8daa69e327297fa57a68
lriki <lriki.net@gmail.com>
2022-08-24 22:57:32 +0900

    #7 Editor key

--- ts/rmmz/Input.ts
+++ ts/rmmz/Input.ts
@@ -1,8 +1,9 @@
 import { FilterGUI } from "../core/FilterGUI";
+import { paramEditorKey } from "./PluginParameters";
 
 const _Input_onKeyDown = Input._onKeyDown;
 Input._onKeyDown = function(event: any) {
-    if (FilterGUI.isPlaytest() && event.key == "F11") {
+    if (FilterGUI.isPlaytest() && event.key == paramEditorKey) {
         FilterGUI.toggle();
     }
     else {
--- ts/rmmz/PluginParameters.ts
+++ ts/rmmz/PluginParameters.ts
@@ -1,6 +1,16 @@
 
+function getString(key: string, defaultValue: string): string {
+    if (typeof PluginManager == "undefined") return defaultValue;
+    const v = PluginManager.parameters(pluginName)[key];
+    if (v === undefined) return defaultValue;
+    return v ?? "";
+}
+
 // プラグイン名
 export const pluginName: string = "LN_FilmicFilter";
 
 // アニメーション "強さ" を フックするときの値
 export var paramAnimationFlushHookValue = 1;
+
+// GUI表示キー
+export var paramEditorKey = getString("EditorKey", "F11");
